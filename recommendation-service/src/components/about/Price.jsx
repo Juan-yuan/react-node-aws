5f0b2a795c39393211840565a057c16cb1d170e2
Kitty Yuan <kitty.yuan@kindredgroup.com>
2023-10-14 21:31:51 +1100

    feat: update delivery page

--- recommendation-service/src/components/about/Price.jsx
+++ recommendation-service/src/components/about/Price.jsx
@@ -5,15 +5,15 @@ export const Price = ({ items, Heading }) => {
   return (
     <>
       <div className='about_price'>
-        <Heading title='Fair Price' />
+        <Heading title='Delivery to your home' />
         <div className='about_price_content'>
           {items.price.map((val) => (
             <div className='cards'>
               <i>{val.icon}</i>
-              <h3>{val.title}</h3>
+              <h5>{val.title}</h5>
               <p>{val.desc}</p>
-              <h1>${val.pri} </h1>
-              <p>per price</p>
+              <h3>${val.pri} </h3>
+              <p>per receipt</p>
 
               <button className='button'>
                 <BiLockOpen className='icons' />
