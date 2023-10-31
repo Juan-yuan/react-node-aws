42f401cde1a474caf5c60e2e2eea7fe2bd2bbd48
Kitty Yuan <kitty.yuan@kindredgroup.com>
2023-11-01 09:29:07 +1100

    feat: main comment out fetch api

--- recommendation-service/src/components/about/Recommendation.jsx
+++ recommendation-service/src/components/about/Recommendation.jsx
@@ -3,36 +3,36 @@ import axios from "axios"
 
 export const Recommendation = ({ items, Heading, userId }) => {
   const [recommendations, setRecommendations] = useState([])
-  const API_URL = `https://gyoxambmvd.execute-api.ap-southeast-2.amazonaws.com/test?userId=${userId}`
+  //const API_URL = `https://gyoxambmvd.execute-api.ap-southeast-2.amazonaws.com/test?userId=${userId}`
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.post(API_URL)
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios.post(API_URL)
   
-        if (response.data && response.data.recommendations) {
-          const userRecommendations = response.data.recommendations?.filter(item => item[userId])
-          if (userRecommendations.length > 0) {
-            setRecommendations(userRecommendations[0][userId])
-          } else { 
-            return
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    getData();
-  }, []);
+  //       if (response.data && response.data.recommendations) {
+  //         const userRecommendations = response.data.recommendations?.filter(item => item[userId])
+  //         if (userRecommendations.length > 0) {
+  //           setRecommendations(userRecommendations[0][userId])
+  //         } else { 
+  //           return
+  //         }
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+  //   getData();
+  // }, []);
   
-  console.log({
-    "User ID:": userId,
-    "Recommendations:": recommendations
-  })
-  
+  // console.log({
+  //   "User ID:": userId,
+  //   "Recommendations:": recommendations
+  // })
+
   return (
     <>
-      { !!(recommendations.length) > 0 && <Heading title='Top Recommendations' /> }
+      {/* { !!(recommendations.length) > 0 && <Heading title='Top Recommendations' /> }
       <div className='recommendation_details_content'>
         {recommendations?.map((val, index) => (
           <div className='recommendation_details_content_box' key={`${val}-${index}-recommendation`}>
@@ -44,7 +44,8 @@ export const Recommendation = ({ items, Heading, userId }) => {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
+      <div></div>
     </>
   )
 }
